283688bb70868542ceeb09a7c25cedb5b3fc2c32
Andrey <andrey.derets2017@gmail.com>
2023-10-23 21:04:16 +0300

    finished basic layout for main page

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -2,6 +2,12 @@ import React from "react";
 import classes from "./Main.module.css";
 
 import populars_icon from "../../assets/main/populars.svg";
+import author1_avatar from "../../assets/main/authors/author1.png";
+import author2_avatar from "../../assets/main/authors/author2.png";
+import author3_avatar from "../../assets/main/authors/author3.png";
+import author4_avatar from "../../assets/main/authors/author4.png";
+import author5_avatar from "../../assets/main/authors/author5.png";
+import author6_avatar from "../../assets/main/authors/author6.png";
 
 const Main = () => {
   return (
@@ -51,9 +57,35 @@ const Main = () => {
               <span>Рейтинг</span>
               <span>Статус</span>
             </div>
-            <div></div>
+            <div className={classes.fanfiction__descriptionBlocks}>
+              <div className={classes.fanfiction__descriptionBlock}>
+                <h3>Автор:</h3>
+                <p>автор</p>
+              </div>
+              <div className={classes.fanfiction__descriptionBlock}>
+                <h3>Позначки:</h3>
+                <span>Позначка</span>
+                <span>Позначка</span>
+                <span>Позначка</span>
+              </div>
+              <div className={classes.fanfiction__descriptionBlock}>
+                <h3>Розмір:</h3>
+                <p>1672 слова</p>
+              </div>
+              <div className={classes.fanfiction__descriptionBlock}>
+                <h3>Автор:</h3>
+                <p>10.09.2021</p>
+              </div>
+              <div className={classes.fanfiction__description}>
+                Lorem ipsum, dolor sit amet consectetur adipisicing elit. Dolor
+                suscipit quia reiciendis ipsa amet optio beatae deserunt itaque.
+                Vel tempore quos recusandae? Eaque reprehenderit expedita nulla
+                recusandae reiciendis non ut.
+              </div>
+            </div>
           </div>
         </div>
+
         <div className={classes.fanfiction}>
           <div className={classes.fanfiction__label}>
             <img src="" alt="" />
@@ -65,11 +97,85 @@ const Main = () => {
               <span>Рейтинг</span>
               <span>Статус</span>
             </div>
-            <div></div>
+            <div className={classes.fanfiction__descriptionBlocks}>
+              <div className={classes.fanfiction__descriptionBlock}>
+                <h3>Автор:</h3>
+                <p>автор</p>
+              </div>
+              <div className={classes.fanfiction__descriptionBlock}>
+                <h3>Позначки:</h3>
+                <span>Позначка</span>
+                <span>Позначка</span>
+              </div>
+              <div className={classes.fanfiction__descriptionBlock}>
+                <h3>Розмір:</h3>
+                <p>11022 слова</p>
+              </div>
+              <div className={classes.fanfiction__descriptionBlock}>
+                <h3>Автор:</h3>
+                <p>11.09.2022</p>
+              </div>
+              <div className={classes.fanfiction__description}>
+                Lorem ipsum, dolor sit amet consectetur adipisicing elit. Dolor
+                suscipit quia reiciendis ipsa amet optio beatae deserunt itaque.
+                Vel tempore quos recusandae? Eaque reprehenderit expedita nulla
+                recusandae reiciendis non ut.
+              </div>
+            </div>
           </div>
         </div>
       </div>
-      <div className={classes.right__block}>Right</div>
+      <div className={classes.right__block}>
+        <div className={classes.popular_authors}>
+          <div className={classes.popular_authorsTitle}>
+            <img src={populars_icon} alt="" />
+            <p>Популярні автори</p>
+          </div>
+
+          <div className={classes.popular_author}>
+            <img src={author1_avatar} alt="" />
+            <div className={classes.author_name}>
+              <h4>Carter Passaquindici Arcand</h4>
+              <p>@CarterPassaquindiciArcand</p>
+            </div>
+          </div>
+          <div className={classes.popular_author}>
+            <img src={author2_avatar} alt="" />
+            <div className={classes.author_name}>
+              <h4>Paityn George</h4>
+              <p>@PaitynGeorge</p>
+            </div>
+          </div>
+          <div className={classes.popular_author}>
+            <img src={author3_avatar} alt="" />
+            <div className={classes.author_name}>
+              <h4>Cheyenne Vaccaro</h4>
+              <p>@CheyenneVaccaro</p>
+            </div>
+          </div>
+          <div className={classes.popular_author}>
+            <img src={author4_avatar} alt="" />
+            <div className={classes.author_name}>
+              <h4>Kianna Septimus</h4>
+              <p>@KiannaSeptimus</p>
+            </div>
+          </div>
+          <div className={classes.popular_author}>
+            <img src={author5_avatar} alt="" />
+            <div className={classes.author_name}>
+              <h4>Cheyenne Press</h4>
+              <p>@CheyennePress</p>
+            </div>
+          </div>
+          <div className={classes.popular_author}>
+            <img src={author6_avatar} alt="" />
+            <div className={classes.author_name}>
+              <h4>James Vetrovs</h4>
+              <p>@JamesVetrovs</p>
+            </div>
+          </div>
+        </div>
+      </div>
     </section>
   );
 };
